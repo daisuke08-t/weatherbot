803940d44360bcbb7530bc4fa2d742426add320d
dai03 <d.taniguchi0803@gmail.com>
2020-06-26 10:46:57 +0000

    open_weather_map api

---
+++ config/open_weather.rb
@@ -0,0 +1,10 @@
+CITY = "Tokyo,JP"
+BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+require "json"
+require "open-uri"
+
+response = open(BASE_URL + "weather?q=#{CITY}&APPID=#{OPEN_API_KEY}")
+data = JSON.pretty_generate(JSON.parse(response.read))
+
+today_weather = data["weather"][0]["description"]
